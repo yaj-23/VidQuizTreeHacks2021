e03a09351cb2ac92940b4b72e71391bac86fd50a
t32rahman <t10rahman@ryerson.ca>
2021-02-14 02:18:24 -0500

    Added search functionality

--- transcribe/src/index.js
+++ transcribe/src/index.js
@@ -8,8 +8,9 @@ const PORT = 3000;
 app.use(cors());
 
 app.get('/test', async(req, res) => {
-    var videoURL = decodeURIComponent("youtube.com/api/timedtext?v=bXTsjwyMKr0\u0026asr_langs=de,en,es,fr,it,ja,ko,nl,pt,ru\u0026caps=asr\u0026exp=xftt\u0026xorp=true\u0026xoaf=5\u0026hl=en\u0026ip=0.0.0.0\u0026ipbits=0\u0026expire=1613273319\u0026sparams=ip,ipbits,expire,v,asr_langs,caps,exp,xorp,xoaf\u0026signature=61721DE3108AC634509B1631F2C5293A63361431.ADC4C4CF6FA73C34405C8B7F2784971D05E0B33D\u0026key=yt8\u0026kind=asr\u0026lang=en");
-    var url = "http://localhost:3000/transcribe/" + encodeURIComponent(videoURL);
+    var videoURL = decodeURIComponent("youtube.com/api/timedtext?v=bXTsjwyMKr0\u0026asr_langs=de,en,es,fr,it,ja,ko,nl,pt,ru\u0026caps=asr\u0026exp=xftt\u0026xorp=true\u0026xoaf=5\u0026hl=en\u0026ip=0.0.0.0\u0026ipbits=0\u0026expire=1613302100\u0026sparams=ip,ipbits,expire,v,asr_langs,caps,exp,xorp,xoaf\u0026signature=C3898F27A440C2230551C20C5EC92F4C719C82A7.8EE62818C71C22EA5058C08C55E43A699B394968\u0026key=yt8\u0026kind=asr\u0026lang=en");
+    var targetWord = "china"
+    var url = "http://localhost:3000/transcribe/" + targetWord + "/" + encodeURIComponent(videoURL);
     await axios({
         method: "GET",
         url: url,
@@ -18,7 +19,7 @@ app.get('/test', async(req, res) => {
         }
     }).then(response => {
         // console.log(response.data);
-        res.json(response.data);
+        res.send(response.data);
     });
 
 })
@@ -90,9 +91,11 @@ app.get('/', async (req, res) => {
     res.send("Hello Wolrd");
 })
  
-app.get('/transcribe/:url', async(req, res) => {
+app.get('/transcribe/:targetWord/:url', async(req, res) => {
     baseURL = decodeURIComponent(req.params.url);
     var url = "https://" + baseURL + "&fmt=json3";
+
+    var targetWord = req.params.targetWord;
 
     var data;
     await axios({
@@ -106,24 +109,40 @@ app.get('/transcribe/:url', async(req, res) => {
     });
 
     var events = data['events'];
-    var words = [];
+    // var words = [];
+    var times = []
     for (var i = 0; i < events.length; i++) {
         if (!('segs' in events[i])){
             continue;
         }
         for (var j = 0; j < events[i]['segs'].length; j++) {
-            try {
-                words.push(events[i]['segs'][j]['utf8']);
+            // try {
+            //     words.push(events[i]['segs'][j]['utf8']);
+            // }
+            // catch(err) {
+            //     console.log(err.message);
+            //     res.send(err.message);
+            // }
+
+            if (events[i]['segs'][j]['utf8'].trim().localeCompare(targetWord.toLowerCase().trim()) == 0) {
+                // console.log(events[i]['segs'][j]['utf8']);
+                // console.log("Found");
+                // console.log(events[i]['tStartMs']);
+                var time = events[i]['tStartMs'];
+                var fmtTime = parseInt(time / 60000) + ":" + parseInt((time / 1000) % 60);
+                times.push(fmtTime);
+                // console.log("Found");
             }
-            catch(err) {
-                console.log(err.message);
-                res.send(err.message);
-            }
-            
+            // if (events[i]['segs'][j]['utf8'] === targetWord) {
+            //     // console.log(events[i]['segs'][j]['utf8']);
+                
+            //     console.log("Found");
+            //     console.log(events[i]['tStartMs']);
+            // }
         }
     }
-    var text = words.join(" ");
-    res.send(text);
+    // var text = words.join(" ");
+    res.json({"times": times});
 
 })
  
